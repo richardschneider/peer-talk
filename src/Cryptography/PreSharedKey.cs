cc69a854680256a59a612ede0c005a9ff770496a
Richard Schneider <makaretu@gmail.com>
2019-04-16 19:42:00 +1200

    feat: pre-shared key

---
+++ src/Cryptography/PreSharedKey.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Ipfs;
+
+namespace PeerTalk.Cryptography
+{
+    /// <summary>
+    ///   A secret (symmetric) key shared among some entities.
+    /// </summary>
+    /// <remarks>
+    ///   This is specifically used for nodes in a private network.
+    /// </remarks>
+    /// <seealso href="https://github.com/libp2p/specs/blob/master/pnet/Private-Networks-PSK-V1.md"/>
+    public class PreSharedKey
+    {
+        const string codecName = "/key/swarm/psk/1.0.0/";
+
+        /// <summary>
+        ///   The key value.
+        /// </summary>
+        /// <value>
+        ///   The value as a byte array.
+        /// </value>
+        public byte[] Value { get; set; }
+
+        /// <summary>
+        ///   The length of the key's value.
+        /// </summary>
+        /// <value>
+        ///   The length in bits.
+        /// </value>
+        public int Length { get { return Value?.Length * 8 ?? 0; } }
+
+        /// <summary>
+        ///   Generate a new value of the specified length.
+        /// </summary>
+        /// <param name="length">
+        ///   The length in bits of the new key value, defaults to 256.
+        /// </param>
+        /// <returns>
+        ///   <b>this</b> for a fluent design.
+        /// </returns>
+        public PreSharedKey Generate(int length = 256)
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                Value = new byte[length / 8];
+                rng.GetBytes(Value);
+            }
+            return this;
+        }
+
+        /// <summary>
+        ///   Write the key to text stream.
+        /// </summary>
+        /// <param name="text">
+        ///   A text writer.
+        /// </param>
+        /// <param name="format">
+        ///   Determines how the key <see cref="Value"/> is formatted.  Can be
+        ///   "base16" or "base64".  Defaults to "base16".
+        /// </param>
+        /// <remarks>
+        ///   The key is writen as three lines 
+        ///   (1) the codec name "/key/swarm/psk/1.0.0/"
+        ///   (2) the base encoding  "/base16/" or "/base64/", 
+        ///   (3) the key value in the base encoding
+        /// </remarks>
+        public void Export(TextWriter text, string format = "base16")
+        {
+            text.WriteLine(codecName);
+            text.Write("/");
+            text.Write(format);
+            text.WriteLine("/");
+            switch (format)
+            {
+                case "base16":
+                    text.WriteLine(Value.ToHexString());
+                    break;
+                case "base64":
+                    text.WriteLine(Value.ToBase64NoPad());
+                    break;
+                default:
+                    throw new Exception($"Unknown encoding '{format}'.");
+            }
+        }
+
+        /// <summary>
+        ///   Read the key from the text stream.
+        /// </summary>
+        /// <param name="text">
+        ///   A text reader.
+        /// </param>
+        public void Import(TextReader text)
+        {
+            if (text.ReadLine() != codecName)
+                throw new FormatException($"Expected '{codecName}'.");
+            switch (text.ReadLine())
+            {
+                case "/base16/":
+                    Value = text.ReadLine().ToHexBuffer();
+                    break;
+                case "/base64/":
+                    Value = text.ReadLine().FromBase64NoPad();
+                    break;
+                default:
+                    throw new FormatException("Unknown base encoding.");
+            }
+        }
+    }
+}
---
+++ test/Cryptography/PreSharedKeyTest.cs
@@ -0,0 +1,98 @@
+﻿using Ipfs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace PeerTalk.Cryptography
+{
+    [TestClass]
+    public class PreSharedKeyTest
+    {
+        [TestMethod]
+        public void Defaults()
+        {
+            var psk = new PreSharedKey();
+            Assert.IsNull(psk.Value);
+            Assert.AreEqual(0, psk.Length);
+        }
+
+        [TestMethod]
+        public void LengthInBits()
+        {
+            var psk = new PreSharedKey { Value = new byte[] { 1, 2 } };
+            Assert.AreEqual(16, psk.Length);
+        }
+
+        [TestMethod]
+        public void Generate()
+        {
+            var psk = new PreSharedKey().Generate();
+            Assert.IsNotNull(psk.Value);
+            Assert.AreEqual(256, psk.Length);
+        }
+
+        [TestMethod]
+        public void Export_Base16()
+        {
+            var psk1 = new PreSharedKey().Generate();
+            var s = new StringWriter();
+            psk1.Export(s, "base16");
+
+            var psk2 = new PreSharedKey();
+            psk2.Import(new StringReader(s.ToString()));
+            CollectionAssert.AreEqual(psk1.Value, psk2.Value);
+        }
+
+        [TestMethod]
+        public void Export_Base64()
+        {
+            var psk1 = new PreSharedKey().Generate();
+            var s = new StringWriter();
+            psk1.Export(s, "base64");
+
+            var psk2 = new PreSharedKey();
+            psk2.Import(new StringReader(s.ToString()));
+            CollectionAssert.AreEqual(psk1.Value, psk2.Value);
+        }
+
+        [TestMethod]
+        public void Export_Base16_is_default()
+        {
+            var psk = new PreSharedKey().Generate();
+            var s1 = new StringWriter();
+            var s2 = new StringWriter();
+            psk.Export(s1);
+            psk.Export(s2, "base16");
+            Assert.AreEqual(s1.ToString(), s2.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Export_BadBase()
+        {
+            var psk = new PreSharedKey().Generate();
+            var s = new StringWriter();
+            psk.Export(s, "bad");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Import_BadCodec()
+        {
+            var s = new StringReader("/bad/codec/");
+            new PreSharedKey().Import(s);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void Import_BadBase()
+        {
+            var s = new StringReader("/key/swarm/psk/1.0.0/\n/base128/");
+            new PreSharedKey().Import(s);
+        }
+    }
+}
