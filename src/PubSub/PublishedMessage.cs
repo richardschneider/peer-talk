4720186e625ea7dee19222faec3a53fbd90d7f66
Richard Schneider <makaretu@gmail.com>
2019-05-11 13:29:46 +1200

    feat: pubsub messages

---
+++ src/PubSub/PublishedMessage.cs
@@ -0,0 +1,90 @@
+﻿using Ipfs;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerTalk.PubSub
+{
+    /// <summary>
+    ///  A published messaged for a topic(s).
+    /// </summary>
+    /// <seealso ref="https://github.com/libp2p/specs/blob/master/pubsub/README.md"/>
+    [ProtoContract]
+    public class PublishedMessage : IPublishedMessage
+    {
+        string messageId;
+
+        /// <inheritdoc />
+        public Peer Sender { get; set; }
+
+        [ProtoMember(1)]
+        byte[] From
+        {
+            get
+            {
+                return Sender?.Id.ToArray();
+            }
+            set
+            {
+                Sender = new Peer { Id = new MultiHash(value) };
+            }
+        }
+
+        /// <inheritdoc />
+        [ProtoMember(4)]
+        public IEnumerable<string> Topics { get; set; }
+
+        /// <inheritdoc />
+        [ProtoMember(3)]
+        public byte[] SequenceNumber { get; set; }
+
+        /// <inheritdoc />
+        [ProtoMember(2)]
+        public byte[] DataBytes { get;  set; }
+
+        /// <inheritdoc />
+        public Stream DataStream
+        {
+            get
+            {
+                return new MemoryStream(DataBytes, false);
+            }
+        }
+
+        /// <summary>>
+        ///   NOT SUPPORTED, use <see cref="MessageId"/>.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///   A published message does not have a content id.
+        /// </exception>
+        public Cid Id => throw new NotImplementedException();
+
+        /// <summary>
+        ///   A universally unique id for the message.
+        /// </summary>
+        /// <value>
+        ///   The sender's ID concatenated with the <see cref="SequenceNumber"/>.
+        /// </value>
+        public string MessageId
+        {
+            get
+            {
+                if (messageId == null)
+                {
+                    messageId = Sender.Id.ToBase58() + SequenceNumber.ToHexString();
+                }
+                return messageId;
+            }
+        }
+
+        /// <inheritdoc />
+        public long Size
+        {
+            get { return DataBytes.Length; }
+        }
+    }
+}
---
+++ test/PubSub/PublishedMessageTest.cs
@@ -0,0 +1,61 @@
+﻿using Ipfs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtoBuf;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PeerTalk.PubSub
+{
+    
+    [TestClass]
+    public class PublishedMessageTest
+    {
+        Peer self = new Peer { Id = "QmXK9VBxaXFuuT29AaPUTgW3jBWZ9JgLVZYdMYTHC6LLAH" };
+        Peer other = new Peer { Id = "QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ" };
+
+        [TestMethod]
+        public void RoundTrip()
+        {
+            var a = new PublishedMessage
+            {
+                Topics = new string[] { "topic" },
+                Sender = self,
+                SequenceNumber = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8},
+                DataBytes = new byte[] { 0, 1, 0xfe, 0xff }
+            };
+            var ms = new MemoryStream();
+            Serializer.Serialize(ms, a);
+            ms.Position = 0;
+            var b = Serializer.Deserialize<PublishedMessage>(ms); ;
+
+            CollectionAssert.AreEqual(a.Topics.ToArray(), b.Topics.ToArray());
+            Assert.AreEqual(a.Sender, b.Sender);
+            CollectionAssert.AreEqual(a.SequenceNumber, b.SequenceNumber);
+            CollectionAssert.AreEqual(a.DataBytes, b.DataBytes);
+        }
+
+        [TestMethod]
+        public void MessageID_Is_Unique()
+        {
+            var a = new PublishedMessage
+            {
+                Topics = new string[] { "topic" },
+                Sender = self,
+                SequenceNumber = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                DataBytes = new byte[] { 0, 1, 0xfe, 0xff }
+            };
+            var b = new PublishedMessage
+            {
+                Topics = new string[] { "topic" },
+                Sender = other,
+                SequenceNumber = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+                DataBytes = new byte[] { 0, 1, 0xfe, 0xff }
+            };
+
+            Assert.AreNotEqual(a.MessageId, b.MessageId);
+        }
+    }
+}
