dc0c8fbafee0697db1b2851e7896614b7b87aba0
Richard Schneider <makaretu@gmail.com>
2019-05-17 11:46:49 +1200

    test(FloodRouter): relays the message

--- test/PubSub/FloodRouterTest.cs
+++ test/PubSub/FloodRouterTest.cs
@@ -25,6 +25,12 @@ namespace PeerTalk.PubSub
             Id = "QmXFX2P5ammdmXQgfqGkfswtEVFsZUJ5KeHRXQYCTdiTAb",
             PublicKey = "CAASpgIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCfBYU9c0n28u02N/XCJY8yIsRqRVO5Zw+6kDHCremt2flHT4AaWnwGLAG9YyQJbRTvWN9nW2LK7Pv3uoIlvUSTnZEP0SXB5oZeqtxUdi6tuvcyqTIfsUSanLQucYITq8Qw3IMBzk+KpWNm98g9A/Xy30MkUS8mrBIO9pHmIZa55fvclDkTvLxjnGWA2avaBfJvHgMSTu0D2CQcmJrvwyKMhLCSIbQewZd2V7vc6gtxbRovKlrIwDTmDBXbfjbLljOuzg2yBLyYxXlozO9blpttbnOpU4kTspUVJXglmjsv7YSIJS3UKt3544l/srHbqlwC5CgOgjlwNfYPadO8kmBfAgMBAAE="
         };
+        Peer other1 = new Peer
+        {
+            AgentVersion = "other1",
+            Id = "QmYSj5nkpHaJG6hDof33fv3YHnQfpFTNAd8jZ5GssgPygn",
+            PublicKey = "CAASpgIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC8s23axzV5S/fUoJ1+MT9fH1SzlDwqwdKoIirYAmvHnv6dyoaC7gMeHDJIc2gNnrvpdAoXyPxBS2Oysv/iHzseVi2kYvyU9pD5ZtiorzpV5oOXMfIfgGygXbiIk/DVQWD6Sq8flHY8ht+z69h9JL+Dj/aMfEzY5RoznJkikumoCn7QI6zvPZd9OPd7OyqcCZ31RThtIxrFd0YkHN+VV9pCq4iBfhMt8Ocy0RS/yrqaGE4PX2VsjExBmShEFnTFlhy0Mh4QhBLLquQH0aQEk2s5mZtwh7bKeW84zC0zIGWzcHrwVsHb+Z2/IXDTWNIlNGc/cCV7vAM1EgK1oQVf04NLAgMBAAE=",
+        };
 
         [TestMethod]
         public void Defaults()
@@ -206,7 +212,7 @@ namespace PeerTalk.PubSub
         }
 
         [TestMethod]
-        public async Task Gets_PublishedMessage()
+        public async Task Relays_PublishedMessage()
         {
             var topic = Guid.NewGuid().ToString();
 
@@ -224,15 +230,26 @@ namespace PeerTalk.PubSub
             await swarm2.StartAsync();
             await ns2.StartAsync();
 
+            var swarm3 = new Swarm { LocalPeer = other1 };
+            var router3 = new FloodRouter { Swarm = swarm3 };
+            var ns3 = new NotificationService { LocalPeer = other1 };
+            ns3.Routers.Add(router3);
+            await swarm3.StartAsync();
+            await ns3.StartAsync();
+
             try
             {
-                IPublishedMessage lastMessage = null;
+                IPublishedMessage lastMessage2 = null;
+                IPublishedMessage lastMessage3 = null;
                 await swarm1.StartListeningAsync("/ip4/127.0.0.1/tcp/0");
                 await swarm2.StartListeningAsync("/ip4/127.0.0.1/tcp/0");
+                await swarm3.StartListeningAsync("/ip4/127.0.0.1/tcp/0");
 
                 var cs = new CancellationTokenSource();
-                await ns1.SubscribeAsync(topic, msg => lastMessage = msg, cs.Token);
+                await ns2.SubscribeAsync(topic, msg => lastMessage2 = msg, cs.Token);
+                await ns3.SubscribeAsync(topic, msg => lastMessage3 = msg, cs.Token);
                 await swarm1.ConnectAsync(other);
+                await swarm3.ConnectAsync(other);
 
                 Peer[] peers = new Peer[0];
                 var endTime = DateTime.Now.AddSeconds(3);
@@ -243,21 +260,26 @@ namespace PeerTalk.PubSub
                     await Task.Delay(100);
                     peers = (await ns2.PeersAsync(topic)).ToArray();
                 }
-                CollectionAssert.Contains(peers, self);
+                CollectionAssert.Contains(peers, other1);
 
-                await ns2.PublishAsync(topic, new byte[] { 1 });
+                await ns1.PublishAsync(topic, new byte[] { 1 });
                 endTime = DateTime.Now.AddSeconds(3);
-                while (lastMessage == null)
+                while (lastMessage2 == null || lastMessage3 == null)
                 {
                     if (DateTime.Now > endTime)
                         Assert.Fail("timeout");
                     await Task.Delay(100);
                 }
 
-                Assert.IsNotNull(lastMessage);
-                Assert.AreEqual(other, lastMessage.Sender);
-                CollectionAssert.AreEqual(new byte[] { 1 }, lastMessage.DataBytes);
-                CollectionAssert.Contains(lastMessage.Topics.ToArray(), topic);
+                Assert.IsNotNull(lastMessage2);
+                Assert.AreEqual(self, lastMessage2.Sender);
+                CollectionAssert.AreEqual(new byte[] { 1 }, lastMessage2.DataBytes);
+                CollectionAssert.Contains(lastMessage2.Topics.ToArray(), topic);
+
+                Assert.IsNotNull(lastMessage3);
+                Assert.AreEqual(self, lastMessage3.Sender);
+                CollectionAssert.AreEqual(new byte[] { 1 }, lastMessage3.DataBytes);
+                CollectionAssert.Contains(lastMessage3.Topics.ToArray(), topic);
             }
             finally
             {
@@ -266,6 +288,9 @@ namespace PeerTalk.PubSub
 
                 await swarm2.StopAsync();
                 await ns2.StopAsync();
+
+                await swarm3.StopAsync();
+                await ns3.StopAsync();
             }
         }
     }
